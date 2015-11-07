297aa3c6eb473c1a4f84cb6c2be4bac797250194
Ben Navetta <ben.navetta@gmail.com>
2015-11-07 12:10:13 -0500

    Common interests page

--- jungo/jungo/__init__.py
+++ jungo/jungo/__init__.py
@@ -16,6 +16,7 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('fb', '/fb')
+    config.add_route('common_interests', '/common-interests')
     config.add_route('interest_match', '/interest-match/{username}/{interest_id}')
 
     config.add_route('api_add_user', '/api/user', request_method='POST')
