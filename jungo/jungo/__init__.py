18ecc8404177fef820e3cc08a202484b096e468c
Ben Navetta <ben.navetta@gmail.com>
2015-11-06 19:06:31 -0500

    Facebook login page

--- jungo/jungo/__init__.py
+++ jungo/jungo/__init__.py
@@ -1,6 +1,5 @@
 from pyramid.config import Configurator
 
-from urlparse import urlparse
 from gridfs import GridFS
 import pymongo
 
@@ -13,8 +12,10 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
 
-    db_url = urlparse(settings['mongo_uri'])
-    config.registry.db = pymongo.Connection(host=db_url.hostname, port=db_url.port)
+    db_url = settings['mongo_uri']
+    db_name = settings['mongo_db']
+    config.registry.client = pymongo.MongoClient(db_url)
+    config.registry.db = config.registry.client[db_name]
 
     def add_db(request):
         db = config.registry.db[db_url.path[1:]]
--- jungo/jungo/views.py
+++ jungo/jungo/views.py
@@ -1,6 +1,11 @@
+import logging
+log = logging.getLogger(__name__)
+
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+# @view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/fb_login.pt')
 def my_view(request):
+    log.debug('Hello, World!')
     return {'project': 'jungo'}
